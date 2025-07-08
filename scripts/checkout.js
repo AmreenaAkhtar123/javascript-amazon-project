7eeba7675e28c7e54d07357c39de6f05a484aed1
Amreena <amberkhannn7654@gmail.com>
2025-07-08 14:39:22 +0500

    solving some issue that the update button have in checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,4 @@
-import {cart, removeFromCart, calculateCartQuantity,
-  updateQuantity
-} from '../data/cart.js';
+import {cart, removeFromCart, calculateCartQuantity, updateQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utlis/money.js';
 
@@ -48,12 +46,12 @@ cart.forEach((cartItem)=>{
             Update
           </span>
 
+
           <input class="quantity-input" js-quantity-input-${matchingProduct.id}">
 
           <span class="save-quantity-link link-primary js-save-link"
           data-product-id="${matchingProduct.id}">Save
           </span>
-
 
           <span class="delete-quantity-link link-primary js-delete-link" data-product-id= "${matchingProduct.id}" >
             Delete
@@ -137,13 +135,11 @@ document.querySelectorAll(`.js-delete-link`).forEach((link)=>{
 });
 
 function updateCartQuantity(){
-  
-  const cartQuantity = calculateCartQuantity();
 
+  const cartQuantity = calculateCartQuantity();
 
   document.querySelector(`.js-return-to-home-link`).innerHTML = `${cartQuantity} items`;
 }
-
 
 updateCartQuantity();
 
